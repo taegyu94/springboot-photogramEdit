7b554475d2bfb0c2feafe6f622683d256c6129b4
taegyu94 <kljh1100@gmail.com>
2022-05-09 17:23:42 +0900

    팔로워팔로잉

---
+++ src/main/java/com/cos/photogramstart/web/dto/user/UserProfileDto.java
@@ -0,0 +1,24 @@
+package com.cos.photogramstart.web.dto.user;
+
+import com.cos.photogramstart.domain.user.User;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class UserProfileDto {
+
+	private boolean pageOwnerState;
+	private int imageCount;
+	private User user;
+	private int followerCount;
+	private int followingCount;
+	private boolean subscribeState;
+	
+}
